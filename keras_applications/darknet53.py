34378913e5dd396acaee7b25d08c135dce6f53a7
Darien Schettler <darienschettler@quantiphi.com>
2020-06-10 11:28:07 -0400

    Update darknet53 architecture to be functional and mock it after the ResNet50 architecture in keras_applications

--- keras_applications/darknet53.py
+++ keras_applications/darknet53.py
@@ -130,29 +130,31 @@ def DarkNet53(include_top=True,
               input_shape=None,
               pooling=None,
               classes=1000, ):
-    """Instantiates the ResNet50 architecture.
+    """Instantiates the DarkNet53 architecture.
 
-        Optionally loads weights pre-trained on ImageNet.
-        Note that the data format convention used by the model is
-        the one specified in your Keras config at `~/.keras/keras.json`.
+    Optionally loads weights pre-trained on ImageNet.
+    Note that the data format convention used by the model is
+    the one specified in your Keras config at `~/.keras/keras.json`.
 
-        Args
-            include_top: whether to include the fully-connected
-                layer at the top of the network.
-            weights: one of `None` (random initialization),
-                  'imagenet' (pre-training on ImageNet),
-                  or the path to the weights file to be loaded.
-            input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
-                to use as image input for the model.
-            input_shape: optional shape tuple, only to be specified
-                if `include_top` is False (otherwise the input shape
-                has to be `(224, 224, 3)` (with `channels_last` data format)
-                or `(3, 224, 224)` (with `channels_first` data format).
-                It should have exactly 3 inputs channels,
-                and width and height should be no smaller than 32.
-                E.g. `(200, 200, 3)` would be one valid value.
-            pooling: Optional pooling mode for feature extraction
-                when `include_top` is `False`.
+    Args:
+        include_top (bool, optional): whether to include the
+            fully-connected layer at the top of the network.
+        weights (str, optional): one of ...
+            `None` (random initialization),
+            `imagenet` (pre-training on ImageNet),
+            or the path to the weights file to be loaded.
+        input_tensor (tf.Tensor): Tensorlflow/Keras tensor
+            i.e. output of `layers.Input()` - to use as input for the model.
+        input_shape (tuple, optional): Shape of input
+            only to be specified if `include_top` is False
+            otherwise the input shape has to be either:
+                - `(224, 224, 3)` with `channels_last` data format
+                -  `(3, 224, 224)` with `channels_first` data format
+            It should have exactly 3 inputs channels,and width and height
+            should be no smaller than 32.
+            E.g. `(200, 200, 3)` would be one valid value.
+        pooling (str, optional): The pooling mode for feature extraction
+            when `include_top` is `False`.
                 - `None` means that the output of the model will be
                     the 4D tensor output of the
                     last convolutional block.
@@ -162,22 +164,61 @@ def DarkNet53(include_top=True,
                     the output of the model will be a 2D tensor.
                 - `max` means that global max pooling will
                     be applied.
-            classes: optional number of classes to classify images
-                into, only to be specified if `include_top` is True, and
-                if no `weights` argument is specified.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+            if no `weights` argument is specified.
 
-        Returns:
-            A Keras model instance.
+    Returns:
+        A Keras model instance.
 
-        Raises:
-            ValueError: in case of invalid argument for `weights`,
-                or invalid input shape.
+    Raises:
+        ValueError:
+            - in case of invalid argument for `weights`
+            - in case of invalid input shape.
     """
-    # Input Layer
-    inputs = layers.Input(shape=input_shape)
+
+    # TODO - Understand this better and update if necessary
+    global backend, layers, models, keras_utils
+    backend, layers, models, keras_utils = get_submodules_from_kwargs(kwargs)
+
+    if not (weights in {'imagenet', None} or os.path.exists(weights)):
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization), `imagenet` '
+                         '(pre-training on ImageNet), '
+                         'or the path to the weights file to be loaded.')
+
+    if weights == 'imagenet' and include_top and classes != 1000:
+        raise ValueError('If using `weights` as `"imagenet"` with `include_top`'
+                         ' as true, `classes` should be 1000')
+
+    # TODO - Integrate this _obtain_input_shape function properly
+    # Determine proper input shape
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=224,
+                                      min_size=32,
+                                      data_format=backend.image_data_format(),
+                                      require_flatten=include_top,
+                                      weights=weights)
+
+    # Create input layer using input shape calculated previously or
+    # pass the input_tensor directly as the input layer
+    if input_tensor is None:
+        img_input = layers.Input(shape=input_shape)
+    else:
+        if not backend.is_keras_tensor(input_tensor):
+            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    # TODO - This can probably be removed
+    # Batch Normalization Axis
+    if backend.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
 
     # BLOCK 1 - CONV 1 - [416x416 --> 416x416]
-    x = conv_block(inputs, filters=32, kernel_size=(3, 3), stride=(1, 1))
+    x = conv_block(img_input, filters=32, kernel_size=(3, 3), stride=(1, 1))
 
     # BLOCK 2 - CONV 2 - [416x416 --> 208x208]
     x = self.conv_block(x, filters=64, kernel_size=(3, 3), stride=(2, 2))
@@ -209,6 +250,9 @@ def DarkNet53(include_top=True,
     # BLOCK 11 - RES 5 - [26x26 --> 26x26]
     x = self.res_block(x, filters=512, repetitions=4)
 
+    # TODO - Review this...
+    #   - It could be done in a few different ways...
+    #   - Ensure this is the best way
     if include_top:
         x = layers.AveragePooling2D()(x)
         x = layers.Conv2D(filters=classes,
@@ -220,164 +264,47 @@ def DarkNet53(include_top=True,
                           name=self.get_name("conv2d"))(x)
         x = layers.GlobalAveragePooling2D()(x)
         outputs = layers.Softmax()(x)
+
+    # TODO: Do we want a final globalaveragepooling2d layer if no head is present?
+    #   - Should we use the pooling option like they do in the ResNet50 architecture?
+    #   - i.e.
+    #         if pooling == 'avg':
+    #             outputs = layers.GlobalAveragePooling2D()(x)
+    #         elif pooling == 'max':
+    #             outputs = layers.GlobalMaxPooling2D()(x)
+    #         else:
+    #             outputs = x
     else:
         outputs = layers.GlobalAveragePooling2D()(x)
 
-    return tf.keras.Model(inputs=inputs, outputs=outputs, name="darknet53")
-
-
-def ResNet50(include_top=True,
-             weights='imagenet',
-             input_tensor=None,
-             input_shape=None,
-             pooling=None,
-             classes=1000,
-             **kwargs):
-    """Instantiates the ResNet50 architecture.
-
-    Optionally loads weights pre-trained on ImageNet.
-    Note that the data format convention used by the model is
-    the one specified in your Keras config at `~/.keras/keras.json`.
-
-    # Arguments
-        include_top: whether to include the fully-connected
-            layer at the top of the network.
-        weights: one of `None` (random initialization),
-              'imagenet' (pre-training on ImageNet),
-              or the path to the weights file to be loaded.
-        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
-            to use as image input for the model.
-        input_shape: optional shape tuple, only to be specified
-            if `include_top` is False (otherwise the input shape
-            has to be `(224, 224, 3)` (with `channels_last` data format)
-            or `(3, 224, 224)` (with `channels_first` data format).
-            It should have exactly 3 inputs channels,
-            and width and height should be no smaller than 32.
-            E.g. `(200, 200, 3)` would be one valid value.
-        pooling: Optional pooling mode for feature extraction
-            when `include_top` is `False`.
-            - `None` means that the output of the model will be
-                the 4D tensor output of the
-                last convolutional block.
-            - `avg` means that global average pooling
-                will be applied to the output of the
-                last convolutional block, and thus
-                the output of the model will be a 2D tensor.
-            - `max` means that global max pooling will
-                be applied.
-        classes: optional number of classes to classify images
-            into, only to be specified if `include_top` is True, and
-            if no `weights` argument is specified.
-
-    # Returns
-        A Keras model instance.
-
-    # Raises
-        ValueError: in case of invalid argument for `weights`,
-            or invalid input shape.
-    """
-    global backend, layers, models, keras_utils
-    backend, layers, models, keras_utils = get_submodules_from_kwargs(kwargs)
-
-    if not (weights in {'imagenet', None} or os.path.exists(weights)):
-        raise ValueError('The `weights` argument should be either '
-                         '`None` (random initialization), `imagenet` '
-                         '(pre-training on ImageNet), '
-                         'or the path to the weights file to be loaded.')
-
-    if weights == 'imagenet' and include_top and classes != 1000:
-        raise ValueError('If using `weights` as `"imagenet"` with `include_top`'
-                         ' as true, `classes` should be 1000')
-
-    # Determine proper input shape
-    input_shape = _obtain_input_shape(input_shape,
-                                      default_size=224,
-                                      min_size=32,
-                                      data_format=backend.image_data_format(),
-                                      require_flatten=include_top,
-                                      weights=weights)
-
-    if input_tensor is None:
-        img_input = layers.Input(shape=input_shape)
-    else:
-        if not backend.is_keras_tensor(input_tensor):
-            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
-        else:
-            img_input = input_tensor
-    if backend.image_data_format() == 'channels_last':
-        bn_axis = 3
-    else:
-        bn_axis = 1
-
-    x = layers.ZeroPadding2D(padding=(3, 3), name='conv1_pad')(img_input)
-    x = layers.Conv2D(64, (7, 7),
-                      strides=(2, 2),
-                      padding='valid',
-                      kernel_initializer='he_normal',
-                      name='conv1')(x)
-    x = layers.BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
-    x = layers.Activation('relu')(x)
-    x = layers.ZeroPadding2D(padding=(1, 1), name='pool1_pad')(x)
-    x = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
-
-    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
-
-    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
-
-    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
-
-    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
-    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
-    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
-
-    if include_top:
-        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-        x = layers.Dense(classes, activation='softmax', name='fc1000')(x)
-    else:
-        if pooling == 'avg':
-            x = layers.GlobalAveragePooling2D()(x)
-        elif pooling == 'max':
-            x = layers.GlobalMaxPooling2D()(x)
-        else:
-            warnings.warn('The output shape of `ResNet50(include_top=False)` '
-                          'has been changed since Keras 2.2.0.')
-
-    # Ensure that the model takes into account
-    # any potential predecessors of `input_tensor`.
+    # Ensure that the model takes into account any potential predecessors of `input_tensor`.
     if input_tensor is not None:
         inputs = keras_utils.get_source_inputs(input_tensor)
     else:
         inputs = img_input
-    # Create model.
-    model = models.Model(inputs, x, name='resnet50')
+
+    # Create model
+    model = models.Model(inputs, outputs, name='darknet53')
 
     # Load weights.
     if weights == 'imagenet':
         if include_top:
             weights_path = keras_utils.get_file(
-                'resnet50_weights_tf_dim_ordering_tf_kernels.h5',
+                'darknet53_weights.h5',
                 WEIGHTS_PATH,
                 cache_subdir='models',
                 md5_hash='a7b3fe01876f51b976af0dea6bc144eb')
         else:
             weights_path = keras_utils.get_file(
-                'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                'darknet53_weights_notop.h5',
                 WEIGHTS_PATH_NO_TOP,
                 cache_subdir='models',
                 md5_hash='a268eb855778b3df3c7506639542a6af')
         model.load_weights(weights_path)
+
         if backend.backend() == 'theano':
             keras_utils.convert_all_kernels_in_model(model)
+
     elif weights is not None:
         model.load_weights(weights)
 
